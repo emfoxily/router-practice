50d817d1485297fe13254f9a2a1e9a40c95d3700
emily reno <emilyreno@emilys-MacBook-Pro.local>
2019-02-18 13:42:52 -0500

    installed wrong node package

--- src/App.js
+++ src/App.js
@@ -1,28 +1,66 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/shop">Shop</Link>
+        </li>
+        <li>
+          <Link to="/community">Community</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact Us</Link>
+        </li>
+      </ul>
+
+      <hr />
+
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/shop" component={Shop} />
+      <Route path="/community" component={Community} />
+      <Route path="/contact" component={Contact} />
+    </div>
+  </Router>
+)
+
+const Home = () => (
+  <div>
+    <h1>This is home!</h1>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h1>This is the about!</h1>
+  </div>
+)
+
+const Shop = () => (
+  <div>
+    <h1>This is the shop!</h1>
+  </div>
+)
+
+const Community = () => (
+  <div>
+    <h1>This is the community!</h1>
+  </div>
+)
+
+const Contact = () => (
+  <div>
+    <h1>This is the contact!</h1>
+  </div>
+)
 
 export default App;
